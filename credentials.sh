3920462ab3dfb6143b42adc3d1c881600ae2b57e
Vijay Kumar Mekala <158064650+vijay8564@users.noreply.github.com>
2024-08-18 21:04:45 +0530

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -3,3 +3,4 @@ export ARM_CLIENT_ID="48d66cdb-3567-46fa-a12b-09ebb5bfc9a4"
 export ARM_CLIENT_SECRET="<secrets-are-not-accepted-by-github>"
 export ARM_TENANT_ID="01cfd074-5415-4ccb-8284-ca1f5bc711cd"
 export ARM_SUBSCRIPTION_ID="d9f96f0e-4bc7-4343-80ee-25efb4f45f4b"
+packer build azure-b1s-image.json
