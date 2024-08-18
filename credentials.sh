eacabb13561b95187e69d51cc3659ea09d56f5cf
Vijay Kumar Mekala <158064650+vijay8564@users.noreply.github.com>
2024-08-18 21:02:45 +0530

    Create credentials.sh

---
+++ credentials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ARM_CLIENT_ID="48d66cdb-3567-46fa-a12b-09ebb5bfc9a4"
+export ARM_CLIENT_SECRET=""
+export ARM_TENANT_ID="01cfd074-5415-4ccb-8284-ca1f5bc711cd"
+export ARM_SUBSCRIPTION_ID="d9f96f0e-4bc7-4343-80ee-25efb4f45f4b"
